f7fc3e7ffad4217c680121123382773194a1d7c6
torresgi-ai <torresgiovan@gmail.com>
2026-08-12 07:32:03 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE='mi-plan-de-deudas-v2-1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
+      const copy=resp.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,copy));
+      return resp;
+    }).catch(()=>caches.match('./index.html')))
+  );
+});
