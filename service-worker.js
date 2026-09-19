1bf003d125af08451d714a7d1c5d5cebd815c267
torresgi-ai <torresgiovan@gmail.com>
2026-09-19 12:44:53 -0400

    Actualizar Mi Plan de Deudas a v2.3

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='mi-plan-de-deudas-v2-2-final';
+const CACHE='mi-plan-de-deudas-v2-3';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
